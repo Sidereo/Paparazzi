5ec571dcdf8b85fa8954f96ec7db4dd2c30f9ab5
marion_h <m@sidereo.com>
2015-03-16 13:39:20 +0100

    Implement adapter to display empty image list

--- app/src/main/java/com/sidereo/picturepicker/PicturePickerAdapter.java
+++ app/src/main/java/com/sidereo/picturepicker/PicturePickerAdapter.java
@@ -1,6 +1,8 @@
 package com.sidereo.picturepicker;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,19 +11,40 @@ import android.widget.ImageView;
  * Created by sidereo on 16/03/15.
  */
 public class PicturePickerAdapter extends RecyclerView.Adapter<PicturePickerAdapter.ListItemViewHolder> {
-    @Override
-    public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public static final String LOG = PicturePickerAdapter.class.getSimpleName();
+
+    public static final int DEFAULT_PREVIEW_PICTURE = 10;
+
+    private int localPicturesPreview;
+
+    PicturePickerAdapter() {
+        this(DEFAULT_PREVIEW_PICTURE);
+    }
+
+    PicturePickerAdapter(int localPicturesPreview) {
+        if (localPicturesPreview < 0) {
+            Log.e(LOG, "Invalid number of previewd pictures.");
+            this.localPicturesPreview = DEFAULT_PREVIEW_PICTURE;
+        } else {
+            this.localPicturesPreview = localPicturesPreview;
+        }
+
     }
 
     @Override
-    public void onBindViewHolder(ListItemViewHolder holder, int position) {
+    public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ListItemViewHolder(itemView);
+    }
 
+    @Override
+    public void onBindViewHolder(ListItemViewHolder viewHolder, int position) {
+        viewHolder.picture.setImageResource(R.drawable.ic_image_grey600_48dp);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return localPicturesPreview;
     }
 
     public class ListItemViewHolder extends RecyclerView.ViewHolder {
