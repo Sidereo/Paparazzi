408fdf9a2b15261d3ecc3823fd944253fb33311c
marion_h <m@sidereo.com>
2015-03-16 15:35:02 +0100

    Displayed preview pictures

--- app/src/main/java/com/sidereo/picturepicker/PicturePickerAdapter.java
+++ app/src/main/java/com/sidereo/picturepicker/PicturePickerAdapter.java
@@ -1,6 +1,8 @@
 package com.sidereo.picturepicker;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,33 +10,44 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by sidereo on 16/03/15.
  */
 public class PicturePickerAdapter extends RecyclerView.Adapter<PicturePickerAdapter.ListItemViewHolder> {
     public static final String LOG = PicturePickerAdapter.class.getSimpleName();
 
-    public static final int DEFAULT_PREVIEW_PICTURE = 10;
+    public static final int DEFAULT_PREVIEW_PICTURE_NB = 10;
 
-    private int localPicturesPreview;
     private Context context;
 
+    private int localPreviews;
+    private List<String> localPreviewPaths;
+
     PicturePickerAdapter(Context context) {
-        this(context, DEFAULT_PREVIEW_PICTURE);
+        this(context, DEFAULT_PREVIEW_PICTURE_NB);
     }
 
     PicturePickerAdapter(Context context, int localPicturesPreview) {
         this.context = context;
         if (localPicturesPreview < 0) {
-            Log.e(LOG, "Invalid number of previewd pictures.");
-            this.localPicturesPreview = DEFAULT_PREVIEW_PICTURE;
+            Log.e(LOG, "Invalid number of previewed pictures.");
+            this.localPreviews = DEFAULT_PREVIEW_PICTURE_NB;
         } else {
-            this.localPicturesPreview = localPicturesPreview;
+            this.localPreviews = localPicturesPreview;
         }
 
-        int maxLocalPicturesPreview = RecentPictureFactory.getMaxAvailablePreview(context);
-        if (this.localPicturesPreview > maxLocalPicturesPreview)
-            this.localPicturesPreview = maxLocalPicturesPreview;
+        int maxLocalPreviews = RecentPictureFactory.getMaxAvailablePreview(context);
+        if (this.localPreviews > maxLocalPreviews)
+            this.localPreviews = maxLocalPreviews;
+
+        String[] paths = RecentPictureFactory.getPicturesPath(context, this.localPreviews);
+        if (paths.length != this.localPreviews)
+            this.localPreviews = paths.length;
+        localPreviewPaths = new ArrayList<String>(Arrays.asList(paths));
     }
 
     @Override
@@ -45,12 +58,14 @@ public class PicturePickerAdapter extends RecyclerView.Adapter<PicturePickerAdap
 
     @Override
     public void onBindViewHolder(ListItemViewHolder viewHolder, int position) {
-        viewHolder.picture.setImageResource(R.drawable.ic_image_grey600_48dp);
+        String path = localPreviewPaths.get(position);
+        Bitmap bm = BitmapFactory.decodeFile(path);
+        viewHolder.picture.setImageBitmap(bm);
     }
 
     @Override
     public int getItemCount() {
-        return localPicturesPreview;
+        return localPreviews;
     }
 
     public class ListItemViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/sidereo/picturepicker/RecentPictureFactory.java
+++ app/src/main/java/com/sidereo/picturepicker/RecentPictureFactory.java
@@ -2,13 +2,17 @@ package com.sidereo.picturepicker;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+
+import java.io.File;
 
 /**
  * Created by sidereo on 16/03/15.
  */
 public class RecentPictureFactory {
-    public static final String[] PROJECTIONS_MAX = new String[]{
+    public static final String[] PROJECTIONS = new String[]{
             MediaStore.Images.ImageColumns._ID,
             MediaStore.Images.ImageColumns.DATA,
             MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
@@ -19,12 +23,13 @@ public class RecentPictureFactory {
     public static int getMaxAvailablePreview(Context context) {
         int count = 0;
         Cursor cursor = null;
+
         try {
             cursor = context.getContentResolver()
-                    .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, PROJECTIONS_MAX, null,
-                            null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+                    .query(getUri(), PROJECTIONS, null, null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
 
             count = cursor.getCount();
+
         } finally {
             if (cursor != null) {
                 cursor.close();
@@ -32,4 +37,40 @@ public class RecentPictureFactory {
         }
         return count;
     }
+
+    public static String[] getPicturesPath(Context context, int size) {
+        String[] result = new String[size];
+        Cursor cursor = null;
+
+        try {
+            cursor = context.getContentResolver()
+                    .query(getUri(), PROJECTIONS, null, null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+
+
+            for(int i=0 ; i<size && cursor.moveToNext() ; i++) {
+                result[i] = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA));
+
+                // Check if image exist, otherwise go to the next row
+                File imageFile = new File(result[i]);
+                if (!imageFile.exists())
+                    i--;
+            }
+
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return result;
+    }
+
+    private static Uri getUri() {
+        String state = Environment.getExternalStorageState();
+        if(!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED))
+            return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+
+        return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    }
+
 }
